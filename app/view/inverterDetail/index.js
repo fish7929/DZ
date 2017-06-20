4e401965f09897fcd755c7f62689acac1227f57b
zhaozhigang <zhaozhigan@qbao.com>
2017-06-20 18:16:40 +0800

    xx

--- app/view/inverterDetail/index.js
+++ app/view/inverterDetail/index.js
@@ -21,6 +21,67 @@ class InverterDetail extends React.Component{
         return(
             <Page className="inverter-detail-container">
                 <Header title="逆变器详情" isShowBack={true} />
+                <div className="header-div"><span>逆变器编号</span><span>SW001</span></div>
+                <div className="alarm-div">
+                    <div className="alarm-title">报警数量</div>
+                    <div className="alarm-list">
+                        <div className="alarm-item">
+                            <div className="alarm-icon alarm_1"></div>
+                            <div className="alarm-count">1</div>
+                            <div className="alarm-level">Ⅰ级报警</div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>逆变器功率-温度曲线</div>
+                    <div className="echart-div">
+
+                    </div>
+                </div>
+                <div>
+                    <div>发电量</div>
+                    <div>
+                        <div>
+                            <span>200Kw/h</span>
+                            <span>当日发电量</span>
+                        </div>
+                        <div>
+                            <div>
+                                <span></span>
+                                <span></span>
+                            </div>
+                            <div>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>交流参数</div>
+                    <div>
+                        <div>
+                            <div>A相</div>
+                            <div>
+                                <span>380V</span>
+                                <span>50HZ</span>
+                                <span>25A</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>直流参数</div>
+                    <div>
+                        <div>
+                            <div>MTPPT1</div>
+                            <div>
+                                <span>600V</span>
+                                <span>20A</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Page>
         )
     }
