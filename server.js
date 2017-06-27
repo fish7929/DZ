70b0dd97db21c59d4987c39c4ee7eb22ae0c441f
chaoneng <chaoneng.feng@chilunyc.com>
2017-06-27 13:53:17 +0800

    public merge

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ new WebpackDevServer(webpack(config), {
     colors: true
   },
   isableHostCheck: true,
-  public: process.env.HOST_ADD || '0.0.0.0',
+  public: process.env.PUBLIC || '0.0.0.0',
   // Set this as true if you want to access dev server from arbitrary url.
   // This is handy if you are using a html5 router.
   historyApiFallback: true,
