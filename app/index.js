aa3c798c0336db83cc55b4bb2c2a2f17440f320a
fishYu <kingfish86@163.com>
2017-10-21 21:33:24 +0800

    优化在ios回弹的功能，

--- app/index.js
+++ app/index.js
@@ -8,7 +8,12 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import GlobalVar from "./utils/GlobalVar";
 import { history, store} from './redux'
 import Routers from './router'
-
+import iNoBounce from "./lib/inobounce";
+import iDevice from "./lib/device";
+//修正ios回弹
+if(iDevice.ios()){
+    iNoBounce.enable();
+}
 ReactDOM.render(
     <Provider store={store}>
         <Router history={history} routes={Routers}></Router>
